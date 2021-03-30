4eca5e400bd8bb25cdd23a1478e8634512d9daf4
Martijn Evers <martijn4evers@gmail.com>
2021-03-30 17:50:23 +0200

    [Cleanup] removed dependency on jujuc runner

--- hook/runner.go
+++ hook/runner.go
@@ -2,11 +2,9 @@ package hook
 
 import (
 	"bytes"
-	"net/rpc"
 	osexec "os/exec"
 	"strings"
 
-	"github.com/juju/utils/exec"
 	"gopkg.in/errgo.v1"
 )
 
@@ -20,51 +18,11 @@ type ToolRunner interface {
 	Close() error
 }
 
-var (
-	// execHookTools specifes whether the hook tools should be called
-	// using os/exec (the conventional way). When this is false,
-	// hook tools are invoked directly through the unix-domain socket
-	// (technically breaking abstraction boundaries but 250 times faster
-	// and easier to test)
-	execHookTools = false
-
-	// jujucSymlinks specifies whether we invoke the hook tools by name.
-	// For testing purposes, setting this to false means that an installed
-	// version of jujud can be used to test the hook logic without creating
-	// symbolic links for all the hook tools.
-	//
-	// This only has an effect when useJujudSocket is false.
-	jujucSymlinks = true
-)
-
-type socketToolRunner struct {
-	contextId   string
-	jujucClient *rpc.Client
-}
-
 // newToolRunnerFromEnvironment returns an implementation of ToolRunner
 // that uses a direct connection to the unit agent's socket to
 // run the tools.
 func newToolRunnerFromEnvironment() (ToolRunner, error) {
 	return &execToolRunner{}, nil
-}
-
-// jujucRequest contains the information necessary to run a Command
-// remotely.
-//
-// It is copied from github.com/juju/juju/worker/uniter/context/jujuc so
-// that we can avoid that dependency in non-test code.
-type jujucRequest struct {
-	ContextId   string
-	Dir         string
-	CommandName string
-	Args        []string
-
-	// StdinSet indicates whether or not the client supplied stdin. This is
-	// necessary as Stdin will be nil if the client supplied stdin but it
-	// is empty.
-	StdinSet bool
-	Stdin    []byte
 }
 
 func isUnimplemented(errStr string) bool {
@@ -73,23 +31,10 @@ func isUnimplemented(errStr string) bool {
 
 var ErrUnimplemented = errgo.New("unimplemented hook tool")
 
-func (r *socketToolRunner) Run(cmd string, args ...string) (stdout []byte, err error) {
-	var resp exec.ExecResponse
-	return resp.Stdout, nil
-
-}
-
-func (r *socketToolRunner) Close() error {
-	return nil
-}
-
 type execToolRunner struct{}
 
 func (execToolRunner) Run(cmd string, args ...string) ([]byte, error) {
 	execCmd := cmd
-	if !jujucSymlinks {
-		execCmd = "jujud"
-	}
 	c := osexec.Command(execCmd, args...)
 	c.Args[0] = cmd
 	var errBuf, outBuf bytes.Buffer
